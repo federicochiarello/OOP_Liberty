578ccf20775d687579897157d2801cedfe8aacfc
Daniel Eduardo Contro <dani.contro2@gmail.com>
2020-05-13 01:41:56 +0200

    Update abstask.h
    Aggiornato l'header pari alla scheda trello del 12/05/20

--- model/abstask.h
+++ model/abstask.h
@@ -2,30 +2,41 @@
 #define ABSTASK_H
 
 #include <string>
-//ciaooo
+
+#include <QJsonObject>
+
+#include "datetime.h"
+
 class AbsTask {
 private:
 	std::string							m_label;
-//	std::string							m_content;
-//	QDate								m_eta;
-	AbsTask*					m_parent;
+	std::string							m_desc;
+	DateTime							m_eta;
+	AbsTask*							m_parent;
 public:
-// Constructors and destructors
-										AbsTask(AbsTask* =nullptr);
-										AbsTask(const std::string, AbsTask* =nullptr);
-										AbsTask(const AbsTask&, AbsTask* =nullptr);
+/* Constructors and destructors */
+										AbsTask(AbsTask* p_parent =nullptr);
+										AbsTask(const std::string p_label, AbsTask* p_parent =nullptr);
+										AbsTask(const std::string p_label, const std::string p_desc, AbsTask* p_parent =nullptr);
+										AbsTask(const AbsTask& p_task, AbsTask* p_parent =nullptr);
+										AbsTask(const QJsonObject& p_obj, AbsTask* p_parent =nullptr);
 
 	virtual								~AbsTask();
 
-// Virtual methods
-	virtual void						modifyLabel(std::string);
-	virtual void						modifyContent() =0;
-
-// Get methods
-
-// Set methods
+/* Virtual methods */
+	virtual QJsonObject&				toJson() const =0;
 
 
+/* Get methods */
+	const std::string					getLabel() const;
+	const std::string					getDesc() const;
+	const DateTime						getEta() const;
+	const AbsTask*						getParent() const;
+/* Set methods */
+	void								setLabel(const std::string);
+	void								setDesc(const std::string);
+	void								setParent(const AbsTask*);
+/* Other methods */
 
 };
 
